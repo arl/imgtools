c7361b75df651981632aef65f321ef1ebbf5a38e
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-09-19 01:51:29 +0200

    Better Scanner doc

--- scanner.go
+++ scanner.go
@@ -1,18 +1,16 @@
 package binimg
 
-import (
-	"image"
-	"image/color"
-)
+import "image"
 
 // A Scanner is an object that can scan a rectangular colored region and report
 // wether it is uniform (i.e composed of the same color) or not.
 type Scanner interface {
 
 	// UniformColor reports wether all the pixels of given region are of the color c.
-	UniformColor(r image.Rectangle, c color.Color) bool
+	UniformColor(r image.Rectangle, c Bit) bool
 
 	// Uniform reports wether the given region is uniform. If is the case, the
-	// uniform color is returned.
-	Uniform(r image.Rectangle) (bool, color.Color)
+	// uniform color bit is returned, otherwise the returned Bit is not
+	// significative (always the zero value of Bit).
+	Uniform(r image.Rectangle) (bool, Bit)
 }
