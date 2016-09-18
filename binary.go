720ed4b88e774084a8710e06dfc610fb69d4a896
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-09-18 21:45:32 +0200

    Add package documentation

--- binary.go
+++ binary.go
@@ -1,3 +1,16 @@
+// Package binimg proposes an in-memory binary image format, implementing the
+// image.Image interface, alongside a set of efficient tools to scan
+// rectangular regions of such images. A binary image is an image that has only
+// two possible values for each pixel, generally Black and White, though any
+// two colors can be used.
+//
+// Though the information represented by each pixel could be stored as a single
+// bit, and thus take the smallest memory footprint, choice has been made to
+// represent Bit pixels as byte values, that can either be 0 (Black or Off) or
+// 255 (White or On), mostly for simplicity reasons.
+//
+// Binary images are created either by calling functions such as NewFromImage
+// and NewBinary, or their counterparts accepting a custom binaryModel.
 package binimg
 
 import (
