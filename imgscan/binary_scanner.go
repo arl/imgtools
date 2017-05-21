2e9859b12bf9ac5a19ad768b598e9841e22adf98
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-05-22 00:39:36 +0200

    Cosmetics

--- imgscan/binary_scanner.go
+++ imgscan/binary_scanner.go
@@ -29,31 +29,28 @@ type binaryScanner struct {
 }
 
 // UniformColor reports wether all the pixels of given region are of the color c.
+//
+// If c is not a color ot type binimg.Bit, UniformColor will panic.
 func (s *binaryScanner) UniformColor(r image.Rectangle, c color.Color) bool {
-	// we want the other color for bytes.IndexBytes
-	// Bit zero value is Off
-	var other binimg.Bit
-	if s.Palette.OffColor == c {
-		other = binimg.On
-	}
-
+	// in a binary image, pixel/bytes are 1 or 0, we want the other color for
+	// bytes.IndexBytes
+	other := c.(binimg.Bit).Other().V
 	for y := r.Min.Y; y < r.Max.Y; y++ {
 		i := s.PixOffset(r.Min.X, y)
 		j := s.PixOffset(r.Max.X, y)
-		if bytes.IndexByte(s.Pix[i:j], byte(other)) != -1 {
-			// quit at the first byte that is not 'other'
+		// look for the first pixel that is not c
+		if bytes.IndexByte(s.Pix[i:j], other) != -1 {
 			return false
 		}
 	}
 	return true
 }
 
-// Uniform reports wether the region specified by r is uniform or not, and if
-// that is the case the uniform color is returned. If the region is not
-// uniform, the color is undefined.
+// Uniform reports wether the given region is uniform. If that is the case, the
+// uniform color is returned, otherwise the returned color is nil.
 func (s *binaryScanner) Uniform(r image.Rectangle) (bool, color.Color) {
-	// color of the first pixel (top-left)
-	first := s.At(r.Min.X, r.Min.Y)
+	// bit color of the first pixel (top-left)
+	first := s.BitAt(r.Min.X, r.Min.Y)
 
 	// check if all the pixels of the region are of this color.
 	if s.UniformColor(r, first) {
--- imgscan/scanner.go
+++ imgscan/scanner.go
@@ -13,8 +13,7 @@ type Scanner interface {
 	// UniformColor reports wether all the pixels of given region are of the color c.
 	UniformColor(r image.Rectangle, c color.Color) bool
 
-	// Uniform reports wether the region specified by r is uniform or not, and if
-	// that is the case the uniform color is returned. If the region is not
-	// uniform, the color is undefined.
-	Uniform(r image.Rectangle) (uniform bool, col color.Color)
+	// Uniform reports wether the given region is uniform. If that is the case, the
+	// uniform color is returned, otherwise the returned is nil.
+	Uniform(r image.Rectangle) (bool, color.Color)
 }
