14785559f515e8b0ad2cb035f86d7c8e1f718397
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-05-26 14:21:19 +0200

    imgscan: add TestGrayScannerAverageColor()

--- imgscan/gray_test.go
+++ imgscan/gray_test.go
@@ -96,12 +96,48 @@ func TestGrayScannerIsUniform(t *testing.T) {
 	scanner, err := NewScanner(img)
 	test.Check(t, err)
 	for _, tt := range tests {
+		// FIXME: only for debug, remove it
+		//sub := img.SubImage(image.Rect(tt.minx, tt.miny, tt.maxx, tt.maxy))
 		uniform, col := scanner.IsUniform(image.Rect(tt.minx, tt.miny, tt.maxx, tt.maxy))
 		if uniform != tt.uniform {
+			//t.Logf("sub image %v\n", sub)
 			t.Errorf("want uniform=%v for IsUniform(rect{%d,%d|%d,%d}), got %v", tt.uniform, tt.minx, tt.miny, tt.maxx, tt.maxy, uniform)
 		}
 		if col != tt.col {
+			//t.Logf("sub image %v\n", sub)
 			t.Errorf("want color=%v for IsUniform(rect{%d,%d|%d,%d}), got %v", tt.col, tt.minx, tt.miny, tt.maxx, tt.maxy, col)
+		}
+	}
+}
+
+func TestGrayScannerAverageColor(t *testing.T) {
+	ss := []string{
+		"  0,   0,   0",
+		"122,   0,   0",
+		"  0,  24,  24",
+	}
+
+	var tests = []struct {
+		minx, miny, maxx, maxy int
+		col                    color.Color
+		uniform                bool
+	}{
+
+		{0, 0, 1, 1, color.Gray{0}, true},
+		{0, 0, 3, 3, color.Gray{18}, false},
+		{0, 1, 1, 2, color.Gray{122}, true},
+		{1, 2, 3, 3, color.Gray{24}, true},
+	}
+
+	scanner, err := NewScanner(newGrayFromString(ss))
+	test.Check(t, err)
+	for _, tt := range tests {
+		uniform, col := scanner.AverageColor(image.Rect(tt.minx, tt.miny, tt.maxx, tt.maxy))
+		if uniform != tt.uniform {
+			t.Errorf("want uniform=%v for AverageColor(rect{%d,%d|%d,%d}), got %v", tt.uniform, tt.minx, tt.miny, tt.maxx, tt.maxy, uniform)
+		}
+		if col != tt.col {
+			t.Errorf("want color=%v for AverageColor(rect{%d,%d|%d,%d}), got %v", tt.col, tt.minx, tt.miny, tt.maxx, tt.maxy, col)
 		}
 	}
 }
